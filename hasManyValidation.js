dbe6f4b08c62aea60f8c007b319d74947baa6726
Marcelo Frantz <mzr@certi.org.br>
2017-02-10 16:59:40 -0200

    Add hasMany validation error test

---
+++ hasManyValidation.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize'),
+      sequelize = new Sequelize('', '','', {
+        host: 'localhost',
+        dialect: 'sqlite',
+      });
+
+const throwError = () => {
+  console.log('should print message below');
+  throw new Error('this message should be printed');
+};
+
+const A = sequelize.define('A');
+const B = sequelize.define('B', {
+  name: {
+    type: Sequelize.STRING,
+    validate: { throwError }
+  }
+});
+
+A.hasMany(B);
+B.belongsTo(A);
+
+const a = { Bs: [{name: ''}, {name: ''}]};
+sequelize
+  .sync()
+  .then(() => A.create(a, { include: [{ association: A.associations.Bs }]}))
+  .then(console.log)
+  .catch(console.err);
